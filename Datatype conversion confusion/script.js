cc14610be4f7ac320fd960311f254f6721d2396f
Himanshi Sharma <himanshish456@gmail.com>
2024-02-26 20:34:54 +0530

    datatype conversions confusions

---
+++ Datatype conversion confusion/script.js
@@ -0,0 +1,47 @@
+// let score = 33;
+
+// console.log(score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let name = "Himanshi";
+
+// console.log(name);
+// console.log(typeof name);
+
+let valueInNumber = Number(name);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//****************************************************************************SUMMARY
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+// For boolean conversion
+// 1=> true , 0 => false
+// "" => false , "Himanshi" => true
+
+////************************************************************OPERATIONS */
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); // 32
+
+//////************************************************COMPARISONS OF DATATYPES */
+
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
