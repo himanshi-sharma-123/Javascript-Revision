18df9fddac4cf7d7032de8e32daac479dd681dd4
Himanshi Sharma <himanshish456@gmail.com>
2024-02-29 13:05:26 +0530

    event bubbling, capturing & propagation

---
+++ Event Bubbling & Capturing/script.js
@@ -0,0 +1,91 @@
+// event bubbling
+
+// document.querySelector("#grandparent").addEventListener("click", () => {
+//   console.log("grandparent clicked");
+// });
+
+// document.querySelector("#parent").addEventListener("click", () => {
+//   console.log("parent clicked");
+// });
+
+// document.querySelector("#child").addEventListener("click", () => {
+//   console.log("child clicked");
+// });
+
+//event trickling/capturing
+
+// document.querySelector("#grandparent").addEventListener(
+//   "click",
+//   () => {
+//     console.log("grandparent clicked");
+//   },
+//   true
+// );
+
+// document.querySelector("#parent").addEventListener(
+//   "click",
+//   () => {
+//     console.log("parent clicked");
+//   },
+//   true
+// );
+
+// document.querySelector("#child").addEventListener(
+//   "click",
+//   () => {
+//     console.log("child clicked");
+//   },
+//   true
+// );
+
+// event propagation in bubbling
+// document.querySelector("#grandparent").addEventListener(
+//   "click",
+//   () => {
+//     console.log("grandparent clicked");
+//   },
+//   false
+// );
+
+// document.querySelector("#parent").addEventListener(
+//   "click",
+//   (e) => {
+//     console.log("parent clicked");
+//     e.stopPropagation();
+//   },
+//   false
+// );
+
+// document.querySelector("#child").addEventListener(
+//   "click",
+//   () => {
+//     console.log("child clicked");
+//   },
+//   false
+// );
+
+//event propagation in capturing
+document.querySelector("#grandparent").addEventListener(
+  "click",
+  (e) => {
+    console.log("grandparent clicked");
+    e.stopPropagation();
+  },
+  true
+);
+
+document.querySelector("#parent").addEventListener(
+  "click",
+  () => {
+    console.log("parent clicked");
+  },
+  true
+);
+
+document.querySelector("#child").addEventListener(
+  "click",
+  () => {
+    console.log("child clicked");
+  },
+  true
+);
