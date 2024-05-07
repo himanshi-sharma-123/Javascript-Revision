dd45b61244b8b88184e4567782b34fb6ad125c99
Himanshi Sharma <himanshish456@gmail.com>
2024-05-07 10:56:51 +0530

    js logical and tricky interview qs

--- Javascript Interview/script.js
+++ Javascript Interview/script.js
@@ -291,41 +291,269 @@ z1();
 
 //*******************************CALLBACK FUNCTION****************************** */
 
-setTimeout(function () {
-  console.log("timer");
-}, 5000);
+// setTimeout(function () {
+//   console.log("timer");
+// }, 5000);
 
-function x(y) {
-  console.log("x");
-  y();
-}
+// function x(y) {
+//   console.log("x");
+//   y();
+// }
 
-x(function y() {
-  console.log("y");
-});
+// x(function y() {
+//   console.log("y");
+// });
 
 //********************************************* ISSUES WITH CALLBACKS ****************************************************/
 
 // 1. Callback hell
 
-const cart = ["shoes", "pen", "pant", "cosmetics"];
+// const cart = ["shoes", "pen", "pant", "cosmetics"];
 
-api.creteOrder(cart, function () {
-  api.proceedToPayment(function () {
-    api.showOrderSummary(function () {
-      api.update();
-    });
-  });
-});
+// api.creteOrder(cart, function () {
+//   api.proceedToPayment(function () {
+//     api.showOrderSummary(function () {
+//       api.update();
+//     });
+//   });
+// });
 
 // 2. Inversion of control
 
 //************************************************* PROMISES ******************************************************/
-const promise = createOrder(cart);
-promise.then(function (orderId) {
-  proceedToPayment(orderId);
-});
+// const promise = createOrder(cart);
+// promise.then(function (orderId) {
+//   proceedToPayment(orderId);
+// });
 
-createOrder(cart).then(function (orderId) {
-  return proceedToPayment(orderId);
-});
+// createOrder(cart).then(function (orderId) {
+//   return proceedToPayment(orderId);
+// });
+
+//********************************************* JAVASCRIPT LOGICAL & TRICKY INTERVIEW QUESIONS ******************************************/
+
+// 1.
+function fruit() {
+  name = "banana";
+  console.log(name);
+  // console.log(price);
+
+  var name = "apple";
+  let price = 100; // hoisting will not be applid on let and const
+}
+fruit();
+
+// 2.
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 5000);
+// }
+
+// 3.
+
+console.warn(+true);
+console.warn(typeof +true);
+
+// 4.
+
+console.warn(!"himanshi");
+console.warn(typeof "himanshi");
+
+// 5.
+
+let data = "size";
+const bird = {
+  size: "small",
+};
+console.warn(bird[data]);
+console.warn(bird["size"]);
+console.warn(bird.size);
+console.warn(bird.data);
+
+// 6.
+
+let c = { name: "peter" };
+let d;
+
+d = c;
+c.name = "peter";
+console.log(d.name);
+
+// 7.
+
+var x;
+var x = 10;
+console.log(x);
+
+// 8.
+
+let var1 = 3;
+let var2 = new Number(3);
+
+console.log(var1 == var2);
+console.log(var1 === var2);
+
+// 9.
+
+function fruit() {
+  console.log("Woof!");
+}
+
+fruit.name = "apple";
+
+fruit();
+
+// 10.
+
+function sum(v1, v2) {
+  return v1 + v2;
+}
+console.warn(sum(1, "2"));
+
+// 11.
+
+let number = 0;
+console.log(number++);
+console.log(++number);
+console.log(number);
+
+// 12.
+
+function getAge(...args) {
+  console.log(args); // array
+
+  console.log(typeof args); // object  (for typeof:  In javascript there is nothing like an array only object)
+}
+getAge(21);
+
+// 13.
+
+function gAge() {
+  "use strict";
+  let age = 21;
+  console.log(age);
+}
+gAge();
+
+// 14.
+
+const s = eval("10*10+5");
+console.warn(s);
+
+// 15.
+
+// How long is cool_secret accessible?
+sessionStorage.setItem("cool_secret", 123); // it will be removed once user closed it
+
+// 16.
+
+// const obj = { 1: "a", 2: "b", 3: "c" };
+// console.log(obj.hasOwnProperty("1")); // true
+// console.log(obj.hasOwnProperty(1)); // true
+
+// 17.
+
+const obj = { a: "one", b: "two", a: "repeat" };
+console.log(obj); //{a:"repeat", b:"two"} overwrite
+
+// 18.
+
+// const foo = () => console.log("First");
+// const bar = () => setTimeout(() => console.log("Second"), 1000);
+// const baz = () => console.log("Third");
+// bar();
+// foo();
+// baz();
+// Output: First Third Second
+
+// 19. // In html file bubbling up
+
+// 20.
+
+// const person = { name: "Himanshi" };
+
+// function sayHi(age) {
+//   return `${this.name} is ${age}`;
+// }
+
+// console.log(sayHi.call(person, 21)); // Himanshi is 21
+// console.log(sayHi.bind(person, 21)); // returns fn
+// console.log(sayHi.bind(person, 21)()); // Himanshi is 21
+
+// 21.
+
+// function sayHi() {
+//   return (() => 0)();
+// }
+// console.log(typeof sayHi()); // number
+
+// 22.
+
+function sayHi() {
+  return () => 0;
+}
+console.log(typeof sayHi()); // function
+
+// 23.
+console.log(typeof typeof 1); // string (because typeof 1 is number and typeof number is string)
+
+// 24.
+
+// const numbers = [1, 2, 3];
+// numbers[9] = 11;
+// console.log(numbers);
+
+// 25.
+const numbers = [1, 2, 3];
+numbers[9] = numbers;
+console.log(numbers); // infinite array at 9th position
+
+// 26.
+// Everything in JS is either a...
+// A: primitive or object  // This is the answer
+// B: function or object
+// C: only objects
+// D: number or object
+
+// 27.
+
+console.log(!!null); // false null is negative value then negate it making it true then again negate it making it false
+console.log(!!""); // false
+console.log(!!1); // true
+
+// 28.
+
+// console.warn(setInterval(() => console.log("Hi"), 1000));
+// console.warn(setInterval(() => console.log("Hi"), 1000));
+// console.warn(setInterval(() => console.log("Hi"), 1000));
+
+// output will be infinte times HI HI HI
+
+// 29.
+console.log([..."himanshi"]); // ['h', 'i', 'm', 'a', 'n', 's', 'h', 'i']
+
+// 30.
+// (() => {
+//   let l = (m = 10);
+// })();
+// console.log(typeof l); // undefined
+
+// 31.
+
+// (() => {
+//   let l = (m = 10);
+// })();
+// console.log(typeof m); // number
+
+// 32.
+
+(() => {
+  let l = 10;
+})();
+
+(() => {
+  let l = 10;
+})();
+console.log(typeof l); // undefined
